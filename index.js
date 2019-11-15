792731c5b74293dbdae27ceed5ec2fb3cdbd10d5
Jonathan Wong <jonathan.wong110@gmail.com>
2019-11-15 16:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
 
-function wrapCards(name, event) {
+function writeCards(name, event) {
   let array = [];
   for (let i = 0; i < gifts.length; i++) {
     console.log(`Thank you ${name[i]}, for the wonderful ${event} gift!`);
