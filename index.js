b43922dcde0765f6df158797d9e37eaadddbc808
Jonathan Wong <jonathan.wong110@gmail.com>
2019-11-15 16:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Code your solutions in this file
 
 
-function writeCards(cards) {
-  for (let i = 0; i < cards.length; i++) {
-    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
-  }
-  return cards;
+function writeCards(names) {
 }
 
 function countdown(number) {
