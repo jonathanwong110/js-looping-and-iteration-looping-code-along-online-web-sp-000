a858efc0b61374671264690460123139bbc9f14e
Jonathan Wong <jonathan.wong110@gmail.com>
2019-11-15 16:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 function writeCards(name, event) {
   let array = [];
   for (let i = 0; i < name.length; i++) {
-    array.push(`Thank you ${name[i]}, for the wonderful ${event} gift!`);
+    array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
   }
  
   return array;
